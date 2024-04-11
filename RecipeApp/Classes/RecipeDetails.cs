334d5965ed621a2bf4546b6ee688e1d0c816cbbd
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2024-04-11 13:20:20 +0200

    Add project files.

---
+++ RecipeApp/Classes/RecipeDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Ethan Parsons
+/// ST10299399
+/// PROG6221
+/// </summary>
+ //-----------------------------------------=========------------------------------------//
+namespace RecipeApp.Classes
+{
+    internal class RecipeDetails
+    {
+           
+    }
+}
+ //-----------------------------------------End of file------------------------------------//
