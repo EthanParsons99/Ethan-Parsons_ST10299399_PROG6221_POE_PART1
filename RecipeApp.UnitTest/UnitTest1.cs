db2a91065d9cec09dab623253154a304e1d4fbd3
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2024-05-30 12:06:51 +0200

    Added a method that tests the calorie calculation of a recipe

--- RecipeApp.UnitTest/UnitTest1.cs
+++ RecipeApp.UnitTest/UnitTest1.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeApp.Classes;
 using System;
+using System.Collections.Generic;
 
 namespace RecipeApp.UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RecipeMethodsTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void calculateCaloriesTest()
         {
+            // Arrange
+            var testRecipe = new Recipe
+            {
+                recipeName = "test",
+
+                ingredients = new List<Ingredient>
+                {
+                    new Ingredient("Milk", 1, "cup", 100, "Dairy"),
+                    new Ingredient("Banana", 1, "one", 89, "Fruit"),
+                }
+            };
+
+            RecipeMethods recipeMethods = new RecipeMethods();
+
+            // Act
+            double totalCalories = recipeMethods.calculateCalories(testRecipe);
+
+            // Assert
+            Assert.AreEqual(189, totalCalories);
         }
     }
 }
