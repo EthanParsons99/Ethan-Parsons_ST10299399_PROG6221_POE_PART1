2b2756b703196a6e7b4a1d159acc1e2f6c8dd57f
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2024-06-27 17:57:27 +0200

    Add recipe method

--- WPFRecipeApp/MainWindow.xaml.cs
+++ WPFRecipeApp/MainWindow.xaml.cs
@@ -1,31 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Linq;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
-using RecipeApp.Classes;
 
 
 namespace WPFRecipeApp
 {
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
+        private Recipe selectedRecipeBackup;
         public MainWindow()
         {
             InitializeComponent();
-            recipes = new List<Recipe>();
-            ingredients = new List<Ingredient>();
-            steps = new List<string>();
+            StoredRecipesListBox.ItemsSource = recipes;
+        }
+
+        public class Recipe
+        {
+            public string Name { get; set; }
+            public ObservableCollection<string> Ingredients { get; set; }
+            public ObservableCollection<string> Steps { get; set; }
+
+            public int Calories { get; set; }   
+
+            public Recipe()
+            {
+
+               Ingredients = new ObservableCollection<string>();
+                Steps = new ObservableCollection<string>();
+            }
+        }
+
+
+        private void AddRecipeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Recipe newRecipe = new Recipe
+            {
+                Name = RecipeNameTextBox.Text,
+                Ingredients = new ObservableCollection<string>(IngredientsListBox.Items.Cast<string>()),
+                Steps = new ObservableCollection<string>(StepsListBox.Items.Cast<string>())
+            };
+
+            newRecipe.Calories = CalculateTotalCalories(newRecipe);
+
+            recipes.Add(newRecipe);
+
+            ClearRecipePanel();
+
+        }
+
+        private int CalculateTotalCalories(Recipe recipe)
+        {
+            int totalCalories = 0;
+
+
         }
 
     }
